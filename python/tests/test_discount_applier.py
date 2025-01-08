39de607690f30b66318159688ea73701dbee9670
Iwaz <nathandelorme.pro@gmail.com>
2025-01-08 15:09:02 +0100

    Part 1 Test DiscountApplier

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,10 +1,33 @@
-def test_apply_v1():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v1
-    pass
+import pytest
 
+from discount_applier import DiscountApplier
 
-def test_apply_v2():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v2
-    pass
+class Notifier:
+    def __init__(self):
+        self.notified_users = []
+
+    def notify(self, user, message):
+        print(f"Notification sent to {user}: {message}")
+        self.notified_users.append(user)
+
+@pytest.fixture
+def notifier():
+    notifier = Notifier()
+    return notifier
+
+@pytest.fixture
+def applier(notifier):
+    applier = DiscountApplier(notifier)
+    return applier
+
+def test_apply_v1(applier, notifier):
+    users_to_notify = ["Alice", "Bob", "Charlie"]
+
+    applier.apply_v1(10, users_to_notify)
+    assert notifier.notified_users == users_to_notify
+
+def test_apply_v2(applier, notifier):
+    users_to_notify = ["Alice", "Bob", "Charlie"]
+
+    applier.apply_v2(10, users_to_notify)
+    assert notifier.notified_users == users_to_notify
