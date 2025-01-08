486540d3d7dc16b6d62330cb493bf770f0e7aba1
{Arthur Billebaut} <{arthur.billebaut@efrei.net}>
2025-01-08 16:07:01 +0100

    unification http-client erreur lié au message

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -5,19 +5,7 @@ class User:
     def __init__(self, email):
         self.email = email
 
-class HttpClientV1:
-    def __init__(self):
-        self.users = []
-
-    def add_user(self, user):
-        self.users.append(user)
-
-    def post(self, base_url, request):
-        if request.recipient not in [user.email for user in self.users]:
-            return SendMailResponse(code=404, message="User Not Found")
-        return SendMailResponse(code=200, message="OK")
-
-class HttpClientV2:
+class HttpClient:
     def __init__(self):
         self.users = []
 
@@ -27,41 +15,35 @@ class HttpClientV2:
     def post(self, base_url, request):
         if type(request) != SendMailRequest:
             return SendMailResponse(code=403, message="Bad Request Type")
-        return SendMailResponse(code=503, message="Internal server error")
+        if request.recipient not in [user.email for user in self.users]:
+            return SendMailResponse(code=404, message="User Not Found")
+        if request.body == "will failed":
+            return SendMailResponse(code=503, message="Internal server error")
+        return SendMailResponse(code=200, message="OK")
+
 
 @pytest.fixture
-def http_client_v1():
-    http_client = HttpClientV1()
+def http_client():
+    http_client = HttpClient()
     return http_client
 
 @pytest.fixture
-def mail_sender_v1(http_client_v1):
-    mail_sender = MailSender(http_client_v1)
+def mail_sender(http_client):
+    mail_sender = MailSender(http_client)
     return mail_sender
 
-def test_send_v1(mail_sender_v1, http_client_v1):
+def test_send_v1(mail_sender, http_client):
     alice = User(email="alice@gmail.com")
-    http_client_v1.add_user(alice)
+    http_client.add_user(alice)
     message = "test à la noix"
 
-    response = mail_sender_v1.send_v1(alice, message)
+    response = mail_sender.send_v1(alice, message)
     assert response.code == 200
 
-
-@pytest.fixture
-def http_client_v2():
-    http_client = HttpClientV2()
-    return http_client
-
-@pytest.fixture
-def mail_sender_v2(http_client_v2):
-    mail_sender = MailSender(http_client_v2)
-    return mail_sender
-
-def test_send_v2(mail_sender_v2, http_client_v2):
+def test_send_v2(mail_sender, http_client):
     alice = User(email="alice@gmail.com")
-    http_client_v2.add_user(alice)
-    message = "test à la noix"
+    http_client.add_user(alice)
+    message = "will failed"
 
-    response = mail_sender_v2.send_v2(alice, message)
+    response = mail_sender.send_v2(alice, message)
     assert response.code == 200
