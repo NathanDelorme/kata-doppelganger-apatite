62dab4438332f9cdf4eb6da9abda38a217481365
Iwaz <nathandelorme.pro@gmail.com>
2025-01-08 16:41:41 +0100

    Part 2 SafeCalculator

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,4 +1,5 @@
 from safe_calculator import SafeCalculator
+from unittest.mock import Mock
 
 class Authorizer:
     def __init__(self, value):
@@ -9,4 +10,11 @@ class Authorizer:
 
 def test_divide_should_not_raise_any_error_when_authorized():
     calculator = SafeCalculator(authorizer=Authorizer(True))
+    calculator.add(1, 2)
+
+def test_divide_should_not_raise_any_error_when_authorized_mock():
+    mock = Mock()
+    mock.authorize.return_value = True
+
+    calculator = SafeCalculator(authorizer=mock)
     calculator.add(1, 2)
